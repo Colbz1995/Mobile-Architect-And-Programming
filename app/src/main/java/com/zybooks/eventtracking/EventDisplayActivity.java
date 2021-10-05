366e20864ff9e8f325c0739bc2d898bbee32ad09
Colby Winn <colwinn@gmail.com>
2021-10-05 00:53:04 -0500

    First Commit

---
+++ app/src/main/java/com/zybooks/eventtracking/EventDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.eventtracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_display);
+    }
+}
